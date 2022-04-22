f753f4812e6513b3a074d9a7fc4742ab2688b30b
Nhat Bui <nhatbui1293@gmail.com>
2022-04-22 16:34:26 +0700

    up 22-04-22 16:35

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Learn React JS Nhật Bùi</p>
+        <p>Learn React JS with Nhật Bùi</p>
       </header>
     </div>
   );
